9cf9b0f4341e2d94e908ad4cd57c5e13e3c7738a
Daniel Roldan Amaya <roldaniel89@gmail.com>
2016-07-02 12:04:53 -0500

    Update AssemblyInfo.cs
    Modificacion de las propiedades de Vidly

--- Vidly/Properties/AssemblyInfo.cs
+++ Vidly/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Vidly")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Vidly is a tracking movie application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Vidly")]
